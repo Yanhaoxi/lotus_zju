766677b1f69ffa89ba68e996013795c7313380df
rainoftime <rainoftime@gmail.com>
2025-10-12 12:06:51 +0800

    feat: Add Static Single Information (SSI) and SRAA alias analysis
    - Introduced vSSA for converting LLVM IR to an extended SSA form with sigma functions.
    - Implemented RangeAnalysis for mapping integer variables to their possible value ranges during execution.
    - Added StrictRelationAliasAnalysis to enhance alias analysis capabilities.
    - Updated CMake configurations to include new SRAA and SSI components, ensuring proper linkage.
    - Enhanced dynamic alias analysis tools with new executables for checking and instrumenting alias analysis.
    - Added comprehensive README documentation for the dynamic alias analysis tools.

--- include/IR/SSI/SSI.h
+++ include/IR/SSI/SSI.h
@@ -12,3 +12,85 @@ Static Single Information (SSI) form = SSA + σ-functions
 4. Rename variables again to give unique names to σ results (mirrors SSA renaming).
 
 */
+
+#pragma once
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Analysis/PostDominators.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace ssi
+{
+  struct SigmaPlacement
+  {
+    llvm::Instruction *Term = nullptr; // terminator instruction location
+    llvm::SmallVector<llvm::BasicBlock *, 4> Successors;
+  };
+
+  class SSIFunctionInfo
+  {
+    public:
+      void clear()
+      {
+        Placements.clear();
+      }
+
+      void addPlacement(llvm::Instruction *Term, llvm::ArrayRef<llvm::BasicBlock *> Succs)
+      {
+        SigmaPlacement P;
+        P.Term = Term;
+        P.Successors.assign(Succs.begin(), Succs.end());
+        Placements.push_back(std::move(P));
+      }
+
+      const llvm::SmallVector<SigmaPlacement, 8> &getPlacements() const { return Placements; }
+
+      void dump(const llvm::Function &F) const
+      {
+        llvm::errs() << "[SSI] Function '" << F.getName() << "' has " << Placements.size() << " sigma site(s)\n";
+        for (const auto &P : Placements)
+        {
+          llvm::errs() << "  - at terminator: ";
+          if (P.Term)
+            llvm::errs() << *P.Term << "\n";
+          else
+            llvm::errs() << "<null>\n";
+          for (auto *Succ : P.Successors)
+          {
+            llvm::errs() << "      succ: ";
+            if (Succ && Succ->getName().size())
+              llvm::errs() << Succ->getName();
+            else
+              llvm::errs() << "<unnamed>";
+            llvm::errs() << "\n";
+          }
+        }
+      }
+
+    private:
+      llvm::SmallVector<SigmaPlacement, 8> Placements;
+  };
+
+  extern bool DEBUG;
+
+  class SSIPass : public llvm::FunctionPass
+  {
+    public:
+      static char ID;
+      SSIPass() : llvm::FunctionPass(ID) {}
+      bool runOnFunction(llvm::Function &F) override;
+      void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
+      llvm::StringRef getPassName() const override { return "Static Single Information Construction"; }
+
+      SSIFunctionInfo &getInfo() { return Info; }
+      const SSIFunctionInfo &getInfo() const { return Info; }
+
+    private:
+      void findSigmaSites(llvm::Function &F, llvm::PostDominatorTree &PDT);
+      static bool needsSigmaAt(llvm::ArrayRef<llvm::BasicBlock *> Succs, llvm::PostDominatorTree &PDT);
+
+      SSIFunctionInfo Info;
+  };
+}
---
+++ lib/IR/SSI/SSIPass.cpp
@@ -0,0 +1,72 @@
+#include "IR/SSI/SSI.h"
+
+using namespace llvm;
+
+namespace ssi
+{
+  char SSIPass::ID = 0;
+  bool DEBUG = false;
+
+  static RegisterPass<SSIPass>
+      SSI("ssi", "Static Single Information Construction", false, true);
+
+  void SSIPass::getAnalysisUsage(AnalysisUsage &AU) const
+  {
+    AU.addRequired<PostDominatorTreeWrapperPass>();
+    AU.setPreservesAll();
+  }
+
+  bool SSIPass::runOnFunction(Function &F)
+  {
+    Info.clear();
+    auto &PDT = getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
+    findSigmaSites(F, PDT);
+    if (DEBUG)
+      Info.dump(F);
+    return false;
+  }
+
+  bool SSIPass::needsSigmaAt(ArrayRef<BasicBlock *> Succs, PostDominatorTree &PDT)
+  {
+    if (Succs.size() <= 1)
+      return false;
+    BasicBlock *First = Succs.front();
+    for (BasicBlock *Succ : Succs)
+    {
+      if (!PDT.dominates(PDT.getRootNode(), PDT.getNode(Succ)))
+        return true;
+      // Require that all successors belong to the same post-dominance region;
+      // if any successor is not post-dominated by the same immediate post-dominator
+      // as the others, we conservatively place a sigma.
+      if (Succ != First)
+      {
+        auto *N1 = PDT.getNode(First);
+        auto *N2 = PDT.getNode(Succ);
+        if (!N1 || !N2)
+          return true;
+        auto *IDom1 = N1->getIDom();
+        auto *IDom2 = N2->getIDom();
+        if (IDom1 != IDom2)
+          return true;
+      }
+    }
+    return false;
+  }
+
+  void SSIPass::findSigmaSites(Function &F, PostDominatorTree &PDT)
+  {
+    for (auto &BB : F)
+    {
+      Instruction *TI = BB.getTerminator();
+      if (!TI)
+        continue;
+      SmallVector<BasicBlock *, 4> Succs;
+      for (auto SI = succ_begin(&BB), SE = succ_end(&BB); SI != SE; ++SI)
+        Succs.push_back(*SI);
+      if (needsSigmaAt(Succs, PDT))
+        Info.addPlacement(TI, Succs);
+    }
+  }
+}
+
+
